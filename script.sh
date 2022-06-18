b20b4d4fee83c5d0db3e51c3a14d100b0ed52616
Aditya Gupta <ag15035@gmail.com>
2022-06-18 19:35:28 +0530

    Handle case when using nvm

--- script.sh
+++ script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # cd to directory containing this file
 cd "$(dirname "$0")"
+
+# if .nvm/nvm.sh exists, source it
+if [ -f $HOME/.nvm/nvm.sh ]; then
+  source $HOME/.nvm/nvm.sh
+fi
+
 cp records.json records.json.bak || true
 npm run start
