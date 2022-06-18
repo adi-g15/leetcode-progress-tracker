37d4161b63e590539004a33067ff8163acff658a
Aditya Gupta <ag15035@gmail.com>
2022-06-18 23:49:57 +0530

    action: Push to main branch itself

--- script.sh
+++ script.sh
@@ -12,10 +12,7 @@ npm run start
 
 ### AFTER THIS LINE, IT'S RELEVANT FOR GITHUB ACTION ###
 
-: ${REPO_DIR:="/tmp/repo"}
-
 : ${GIT_REMOTE:=""}
-: ${GIT_BRANCH:="records"}
 
 : ${GITHUB_ACTOR:=""}
 GITHUB_REPO_OWNER=${GITHUB_REPOSITORY%/*}
@@ -25,31 +22,25 @@ ensure_env() {
 	: ${!1:?"env \$${1} is not set"}
 }
 
+# PRE-REQUISITES:
+# 1. GIT_REMOTE be defined containing token
+# 2. npm install already run
 githubaction() {
     ensure_env GIT_REMOTE
-    ensure_env GIT_BRANCH
     ensure_env GITHUB_ACTOR
 
     ## Step 1: Update the records, assuming currently in the cloned directory
-    cp records.json records.json.bak || true
     npm run start
 
-    mkdir -p ${REPO_DIR}
-    cp records.json records.json.bak ${REPO_DIR}/
-
-    ## Step 2: Create a branch, and commit the files
-    cd ${REPO_DIR}
-    git init
-    git checkout --orphan "${GIT_BRANCH}"
-    git add --all
+    ## Step 2: Commit the files
+    git add records.json
     git config user.email "bot.noreply@github.com"
-    git config user.name "Github Bot"
+    git config user.name "adityag-gitbot"
 
-    git commit -m "Updated at $(date +'%Y/%m/%d %H:%M:%S')"
+    git commit -m "Auto-Updated at $(date +'%Y/%m/%d %H:%M:%S')"
 
-    ## Step 3: Push
-    git remote add origin "${GIT_REMOTE}"
-    git push --force --set-upstream origin "${GIT_BRANCH}"
+    ## Step 3: Push, assuming remote 'origin' already set (by default, it is)
+    git push origin main	# Pushing to current branch itself
 }
 
 set -xe
